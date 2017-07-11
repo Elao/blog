a3447ffed63cf9bd7279e148fda1380d8be74f51
Richard Hanna <richard.hanna@elao.com>
2017-07-11 13:00:37 +0200

    Revert "Remove workbox webpack config"
    This reverts commit 374129364c20fb6d9ca398f2e871a183654120cb.

--- themes/2015/webpack.mix.js
+++ themes/2015/webpack.mix.js
@@ -1,3 +1,6 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const WorkboxBuildWebpackPlugin = require('workbox-webpack-plugin');
+
 let mix = require('laravel-mix');
 mix
     .setResourceRoot('/assets/')
@@ -8,4 +11,27 @@ mix
     })
     .js('assets/js/app.js', 'js')
     .sass('assets/sass/app.scss', 'css')
+    .webpackConfig({
+        plugins: [
+            new WorkboxBuildWebpackPlugin({
+                globDirectory: 'static',
+                globPatterns: ['**\/*.{js,css,jpg,svg,png,gif}'],
+                swDest: 'static/service-worker.js',
+                swSrc: 'service-worker.dist.js',
+                templatedUrls: {
+                  '/en/offline/offline/': [
+                    '../../../content/offline/offline.en.md',
+                    '../layouts/offline/single.html'
+                  ]
+                }
+            }),
+            new CopyWebpackPlugin([
+                // copy WorkboxSW production build file to assets/js/workbox-sw.prod.js
+                {
+                  from: require.resolve('workbox-sw'),
+                  to: 'js/workbox-sw.prod.js'
+                }
+            ]),
+        ]
+    })
 ;
