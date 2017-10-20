fc8bcd5a155324de404eebac1c78e75f7b21c600
Richard Hanna <richard.hanna@elao.com>
2017-10-20 14:18:05 +0200

    Build SW only in production env

--- themes/2015/webpack.mix.js
+++ themes/2015/webpack.mix.js
@@ -11,27 +11,32 @@ mix
     })
     .js('assets/js/app.js', 'js')
     .sass('assets/sass/app.scss', 'css')
-    .webpackConfig({
-        plugins: [
-            new WorkboxBuildWebpackPlugin({
-                globDirectory: 'static',
-                globPatterns: ['**\/*.{js,css,jpg,svg,png,gif}'],
-                swDest: 'static/service-worker.js',
-                swSrc: 'service-worker/service-worker.template.js',
-                templatedUrls: {
-                  '/en/offline/offline/': [
-                    '../../../content/offline/offline.en.md',
-                    '../layouts/offline/single.html'
-                  ]
-                }
-            }),
-            new CopyWebpackPlugin([
-                // copy WorkboxSW production build file to assets/js/workbox-sw.prod.js
-                {
-                    from: require.resolve('workbox-sw'),
-                    to: 'js/workbox-sw.prod.js'
-                }
-            ]),
-        ]
-    })
 ;
+
+if (mix.config.inProduction) {
+  mix
+    .webpackConfig({
+      plugins: [
+        new WorkboxBuildWebpackPlugin({
+          globDirectory: 'static',
+          globPatterns: ['**\/*.{js,css,jpg,svg,png,gif}'],
+          swDest: 'static/service-worker.js',
+          swSrc: 'service-worker/service-worker.template.js',
+          templatedUrls: {
+            '/en/offline/offline/': [
+              '../../../content/offline/offline.en.md',
+              '../layouts/offline/single.html'
+            ]
+          }
+        }),
+        new CopyWebpackPlugin([
+          // copy WorkboxSW production build file to assets/js/workbox-sw.prod.js
+          {
+            from: require.resolve('workbox-sw'),
+            to: 'js/workbox-sw.prod.js'
+          }
+        ]),
+      ]
+    })
+  ;
+}
